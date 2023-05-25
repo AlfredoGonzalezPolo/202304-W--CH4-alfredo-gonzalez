1ee842cd31635e11182885a26f812a0481b286fd
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-05-26 00:39:00 +0200

    Change keyboard and keyItem to do the correct way

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -1,11 +1,28 @@
-import { KeyItem } from "./key.item";
+import { KeyItem } from "../keyboard/key.item";
 
 export function Keyboard() {
+  const numbers: string[] = [
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+    "1",
+    "0",
+    "Delete",
+  ];
   return (
-    <div className="keyboard-container">
-      <ol className="keyboard">
-        <KeyItem></KeyItem>
-      </ol>
-    </div>
+    <>
+      <div className="keyboard-container">
+        <ol className="keyboard">
+          {numbers.map((item) => (
+            <KeyItem number={item} key={item}></KeyItem>
+          ))}
+        </ol>
+      </div>
+    </>
   );
 }
--- src/components/keyboard/key.item.tsx
+++ src/components/keyboard/key.item.tsx
@@ -1,38 +1,12 @@
-export function KeyItem() {
+export type PropsType = {
+  number: string;
+};
+
+export function KeyItem({ number }: PropsType) {
   return (
     <>
       <li>
-        <button className="key">1</button>
-      </li>
-      <li>
-        <button className="key">2</button>
-      </li>
-      <li>
-        <button className="key">3</button>
-      </li>
-      <li>
-        <button className="key">4</button>
-      </li>
-      <li>
-        <button className="key">5</button>
-      </li>
-      <li>
-        <button className="key">6</button>
-      </li>
-      <li>
-        <button className="key">7</button>
-      </li>
-      <li>
-        <button className="key">8</button>
-      </li>
-      <li>
-        <button className="key">9</button>
-      </li>
-      <li>
-        <button className="key">0</button>
-      </li>
-      <li>
-        <button className="key big">delete</button>
+        <button className="key">{number}</button>
       </li>
     </>
   );
